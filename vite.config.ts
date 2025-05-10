98859c8a7ee5102fffc72c5cb3de8744640265f9
ohm <ohmp@hotmail.com>
2025-05-10 12:38:09 +0530

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: process.env.NODE_ENV === 'production' ? '/ohmp.github.io/' : '/',
+ // base: process.env.NODE_ENV === 'production' ? '/ohmp.github.io/' : '/',
   //base: '/ohmp.github.io/',
   plugins: [
     react(),
